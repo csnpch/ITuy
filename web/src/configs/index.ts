08375949360d7c17fa765157cc19f7822c56057c
Chitsanuphong Chaihong <chitsanuphongchaihong@outlook.com>
2023-10-16 23:15:29 +0700

    update

--- web/src/configs/index.ts
+++ web/src/configs/index.ts
@@ -1,4 +1,5 @@
 export default {
-    // ITuy_API: 'http://192.168.1.83:4000'
-    ITuy_API: 'http://45.130.141.151:4000' || 'http://localhost:4000'
+    // ITuy_API: 'http://192:4000'
+    // ITuy_API: 'http://45.130.141.151:4000',
+    ITuy_API: 'http://192.168.1.88:4000',
 }
